359aaf21fde231b7827d0f275eb945100d3628e6
Adnan Latif <alatif.bwp@gmail.com>
2021-09-16 17:58:45 +0500

    Replaced with better approach.

--- rec.js
+++ rec.js
@@ -1,14 +1,9 @@
-
 const timesFunc = (repeatTimes, repeatItem, repeated) => {
-      if(repeatTimes === 0) {
+      if(repeatTimes <= 0) {
           return repeated
       }
-      if(repeatTimes < 0) {
-          return []
-      } 
           repeated.push(repeatItem)
-          return timesFunc(repeatTimes - 1, repeatItem)
+          return timesFunc(repeatTimes - 1, repeatItem, repeated)
   }
   
   console.log(timesFunc(3, 3, []))
-  
